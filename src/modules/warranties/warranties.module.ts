54d8f7d4261b8f5fce8012fe94c0e9e36024f629
fazliddin <fazliddinnarkhajayev@gmail.com>
2026-02-15 11:59:25 +0500

    add

--- src/modules/warranties/warranties.module.ts
+++ src/modules/warranties/warranties.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { WarrantyHistoriesRepository } from 'src/shared/repositories/warranty-histories.repository';
 import { WarrantiesController } from './warranties.controller';
 import { WarrantiesService } from './warranties.service';
 
 @Module({
   controllers: [WarrantiesController],
-  providers: [WarrantiesService]
+  providers: [WarrantiesService, WarrantyHistoriesRepository]
 })
-export class WarrantiesModule {}
+export class WarrantiesModule { }
